3384b7849636c0a45328ae8419725c0a7c570cb7
Chad <chadutk@gmail.com>
2021-10-15 10:51:52 -0700

    Add basic chessboard and pieces

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: Board()));
 }
 
-class MyApp extends StatelessWidget {
+class Board extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+  final String title = "Chess";
 
-  // This widget is the root of your application.
+  @override
+  State<StatefulWidget> createState() {
+    return _BoardState();
+  }
+}
+
+class ChessPiece extends StatefulWidget {
+  String source;
+  int xpos;
+  int ypos;
+  ChessPiece(this.source, this.xpos, this.ypos);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ChessPieceState();
+  }
+}
+
+class _ChessPieceState extends State<ChessPiece> {
+  String source = "";
+  int xpos = 0;
+  int ypos = 0;
+  bool visible = true;
+
+  @override
+  initState() {
+    super.initState();
+    source = widget.source;
+    xpos = widget.xpos;
+    ypos = widget.ypos;
+  }
+
+  Widget getImage() {
+    debugPrint("getting image");
+    if (visible) {return FractionallySizedBox(
+      child: Image.asset("assets/images/$source.png"),
+      heightFactor: 1 / 8 * .9,
+      widthFactor: 1 / 8 * .9,
+    ); }
+    else return SizedBox.shrink();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Align(
+        alignment: Alignment((xpos - 5 + .5) / 3.6, (ypos - 5 + .5) / 3.6),
+        child: Visibility(child: Draggable(
+          child: getImage(),
+          childWhenDragging: Image.asset("assets/images/$source.png"),//getImage(),
+         feedback: SizedBox.shrink(),//getImage(),//Image.asset("assets/images/$source.png"),
+          onDraggableCanceled: (velocity, offset) {
+            debugPrint("$visible");
+            visible = true;
+            debugPrint("size" + context.size.toString());
+            debugPrint("$xpos ${offset.dx} ${(context.size as Size).width}");
+            debugPrint("$ypos ${offset.dy} ${(context.size as Size).height}");
+            int nextx = (offset.dx * 8 / (context.size as Size).width + .8).round();
+            int nexty = (offset.dy * 8 / (context.size as Size).height + .8).round();
+            debugPrint("next: $nextx $nexty");
+            if (nextx >= 1 && nextx <= 8 && nexty >= 1 && nexty <= 8) {
+              xpos = nextx;
+              ypos = nexty;
+            }
+            debugPrint("drag canceled!");
+            debugPrint(offset.toString());
+            setState(() {
+              debugPrint("setting state");
+            });
+          },
+          onDragEnd: (details) {
+            debugPrint("Drag End!");
+          },
+          onDragStarted: () {
+            debugPrint("$visible");
+            debugPrint("drag started!");
+            visible = false;
+            setState(() {
+              debugPrint("setting state");
+            });
+          },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        visible: visible),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class _MyHomePageState extends State<Board> {
+  List<ChessPiece> pieces = [];
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  _MyHomePageState() {
+    List<ChessPiece> pawns =
+        List.generate(8, (index) => ChessPiece("pawn", index + 1, 2));
+    pieces = [
+      ChessPiece("rook", 1, 1),
+      ChessPiece("rook", 8, 1),
+      ChessPiece("knight", 2, 1),
+      ChessPiece("knight", 7, 1),
+      ChessPiece("bishop", 3, 1),
+      ChessPiece("bishop", 6, 1),
+      ChessPiece("queen", 4, 1),
+      ChessPiece("king", 5, 1),
+    ];
+    pieces.addAll(pawns);
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<Widget> _createChildren() {
+    List<Widget> result =
+        List.generate(16, (index) => pieces[index]);
+    result.insert(
+        0,
+        Opacity(
+        child: Image.asset(
+          "assets/images/board.png",
+          fit: BoxFit.fill,
+        ),
+    opacity: .5,
+    ));
+    return result;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return Stack(
+      alignment: Alignment.center,
+      children:
+          _createChildren(),
     );
   }
 }
