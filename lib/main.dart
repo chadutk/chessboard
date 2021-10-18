07e65b03f8cbcdd84a67f62183a89bc1928fe03c
Chad <chadutk@gmail.com>
2021-10-17 20:37:48 -0700

    migrated to flame

--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,146 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+
+import 'package:flame/components.dart';
+import 'package:flame/extensions.dart';
+import 'package:flame/game.dart';
+import 'package:flame/geometry.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/material.dart' hide Image, Draggable;
 
 void main() {
-  runApp(MaterialApp(home: Board()));
+  final myGame = MyGame();
+  runApp(
+    GameWidget(game: myGame)
+  );
 }
 
-class Board extends StatefulWidget {
-  const Board({Key? key}) : super(key: key);
-  final String title = "Chess";
+class Board extends SpriteComponent with HasGameRef<MyGame>{
+
+  Future<void> onLoad() async {
+    sprite = await Sprite.load("board.png");
+  }
 
   @override
-  State<StatefulWidget> createState() {
-    return _BoardState();
+  void onGameResize(Vector2 gameSize) {
+    size = gameSize;
+  }
+
+  @override
+  update(double t) {
+    size = gameRef.canvasSize;
+  }
+
+  @override
+  void render(Canvas canvas) {
+    Paint bg = Paint();
+    bg.color = Colors.white;
+    canvas.drawRect(
+        Rect.fromLTRB(0, 0, gameRef.canvasSize.x, gameRef.canvasSize.y),
+        bg);
+    paint.color = Colors.black.withOpacity(.5);
+    super.render(canvas);
   }
 }
 
-class ChessPiece extends StatefulWidget {
+class ChessPiece extends SpriteComponent with Draggable, HasGameRef<MyGame> {
   String source;
-  int xpos;
-  int ypos;
-  ChessPiece(this.source, this.xpos, this.ypos);
+  Vector2 latestDragLocation = Vector2(0,0);
+  Vector2 relativeDragStartPosition = Vector2(0,0);
+
+  ChessPiece(String source) :
+    this.source = source,
+    super();
 
   @override
-  State<StatefulWidget> createState() {
-    return _ChessPieceState();
+  Future<void> onLoad() async {
+    sprite = await Sprite.load(source + ".png");
+  }
+
+  Vector2 toScreen(Vector2 offscreen) {
+    Vector2 transformed = gameRef.canvasSize / 8;
+    transformed.multiply(offscreen + Vector2(0.06, .1));
+    return transformed;
+  }
+
+  Vector2 fromScreen(Vector2 onscreen) {
+    onscreen.divide(gameRef.canvasSize / 8);
+    return onscreen - Vector2(0.06, .1);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    size = gameRef.canvasSize / 8 * .9;
+    position = toScreen(gameRef.getPosition(this));
+  }
+
+  @override
+  bool onDragStart(int id, DragStartInfo info) {
+    relativeDragStartPosition = info.eventPosition.game - toScreen(gameRef.getPosition(this));
+    return false;
+  }
+
+  @override
+  bool onDragUpdate(int id, DragUpdateInfo info) {
+    latestDragLocation = info.eventPosition.game;
+    return false;
+  }
+
+  @override
+  bool onDragEnd(int id, DragEndInfo info) {
+    gameRef.move(
+        this,
+        fromScreen(latestDragLocation - relativeDragStartPosition));
+    return false;
   }
 }
 
-class _ChessPieceState extends State<ChessPiece> {
-  String source = "";
-  int xpos = 0;
-  int ypos = 0;
-  bool visible = true;
+class MyGame extends FlameGame with HasDraggableComponents {
+  var pieceMap = Map();
 
   @override
-  initState() {
-    super.initState();
-    source = widget.source;
-    xpos = widget.xpos;
-    ypos = widget.ypos;
+  void update(double dt) {
+    super.update(dt);
   }
 
-  Widget getImage() {
-    debugPrint("getting image");
-    if (visible) {return FractionallySizedBox(
-      child: Image.asset("assets/images/$source.png"),
-      heightFactor: 1 / 8 * .9,
-      widthFactor: 1 / 8 * .9,
-    ); }
-    else return SizedBox.shrink();
+  addPiece(String src, int x, int y) {
+    ChessPiece piece = ChessPiece(src);
+    pieceMap[piece] = Vector2(x as double, y as double);
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Align(
-        alignment: Alignment((xpos - 5 + .5) / 3.6, (ypos - 5 + .5) / 3.6),
-        child: Visibility(child: Draggable(
-          child: getImage(),
-          childWhenDragging: Image.asset("assets/images/$source.png"),//getImage(),
-         feedback: SizedBox.shrink(),//getImage(),//Image.asset("assets/images/$source.png"),
-          onDraggableCanceled: (velocity, offset) {
-            debugPrint("$visible");
-            visible = true;
-            debugPrint("size" + context.size.toString());
-            debugPrint("$xpos ${offset.dx} ${(context.size as Size).width}");
-            debugPrint("$ypos ${offset.dy} ${(context.size as Size).height}");
-            int nextx = (offset.dx * 8 / (context.size as Size).width + .8).round();
-            int nexty = (offset.dy * 8 / (context.size as Size).height + .8).round();
-            debugPrint("next: $nextx $nexty");
-            if (nextx >= 1 && nextx <= 8 && nexty >= 1 && nexty <= 8) {
-              xpos = nextx;
-              ypos = nexty;
-            }
-            debugPrint("drag canceled!");
-            debugPrint(offset.toString());
-            setState(() {
-              debugPrint("setting state");
-            });
-          },
-          onDragEnd: (details) {
-            debugPrint("Drag End!");
-          },
-          onDragStarted: () {
-            debugPrint("$visible");
-            debugPrint("drag started!");
-            visible = false;
-            setState(() {
-              debugPrint("setting state");
-            });
-          },
-      ),
-        visible: visible),
-    );
-  }
-}
+  Future<void>? onLoad() async {
+    super.onLoad();
+    var board = Board();
+    for(int i = 0; i < 8; i++) {
+      addPiece("pawn", i, 1);
+    }
+    addPiece("rook", 0, 0);
+      addPiece("rook", 7, 0);
+      addPiece("knight", 1, 0);
+      addPiece("knight", 6, 0);
+      addPiece("bishop", 2, 0);
+      addPiece("bishop", 5, 0);
+      addPiece("queen", 3, 0);
+      addPiece("king", 4, 0);
 
-class _BoardState extends State<Board> {
-  List<ChessPiece> pieces = [];
+      pieceMap.keys.forEach((element) {
+        add(element);
+        changePriority(element, 1);
+      });
 
-  _BoardState() {
-    List<ChessPiece> pawns =
-        List.generate(8, (index) => ChessPiece("pawn", index + 1, 2));
-    pieces = [
-      ChessPiece("rook", 1, 1),
-      ChessPiece("rook", 8, 1),
-      ChessPiece("knight", 2, 1),
-      ChessPiece("knight", 7, 1),
-      ChessPiece("bishop", 3, 1),
-      ChessPiece("bishop", 6, 1),
-      ChessPiece("queen", 4, 1),
-      ChessPiece("king", 5, 1),
-    ];
-    pieces.addAll(pawns);
+      add(board);
+      changePriority(board, 0);
   }
 
-  List<Widget> _createChildren() {
-    List<Widget> result =
-        List.generate(16, (index) => pieces[index]);
-    result.insert(
-        0,
-        Opacity(
-        child: Image.asset(
-          "assets/images/board.png",
-          fit: BoxFit.fill,
-        ),
-    opacity: .5,
-    ));
-    return result;
+  void move(ChessPiece piece, Vector2 updated) {
+    updated.round();
+    pieceMap[piece] = updated;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children:
-          _createChildren(),
-    );
+  Vector2 getPosition(ChessPiece piece) {
+    return pieceMap[piece];
   }
 }
